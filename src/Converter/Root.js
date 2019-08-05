a4a984f0e54617a48c90d8b62d5af5fbf9cd8eb5
barrymun <barrymun@tcd.ie>
2019-08-05 13:50:30 +0200

    flags changing depending on selection

--- src/Converter/Root.js
+++ src/Converter/Root.js
@@ -37,6 +37,12 @@ const styles = theme => ({
     divider: {
         width: 10,
     },
+    flagSvgContainer: {
+        height: 30,
+    },
+    flagSvg: {
+        width: 40,
+    },
 });
 let options = Object.keys(currencyJSON).map((key, index) => {
     return {label: currencyJSON[key].name, value: key};
@@ -55,12 +61,13 @@ class Root extends React.Component {
         selectedFromCurrency: {
             label: currencyJSON[this.initialFromCurrency].name,
             value: currencyJSON[this.initialFromCurrency].code,
+            flagSrc: flagJSON[this.initialFromCurrency].src,
         },
         selectedToCurrency: {
             label: currencyJSON[this.initialToCurrency].name,
             value: currencyJSON[this.initialToCurrency].code,
-            src: flagJSON[this.initialToCurrency].src,
-        }
+            flagSrc: flagJSON[this.initialToCurrency].src,
+        },
     };
 
     constructor(props) {
@@ -90,83 +97,109 @@ class Root extends React.Component {
             toAmount,
             exchangeRate,
         } = this.state;
-
         if (exchangeRate == null) return null;
 
         return (
             <div className={classes.container}>
-                <div style={{width: 100}}>
-                    <ReactSVG src={selectedToCurrency.src}/>
-                </div>
-                <Card className={classes.card}>
-                    <div className={classes.select}>
-                        <Select
-                            className="currency"
-                            classNamePrefix="select"
-                            name="selectedFromCurrency"
-                            defaultValue={selectedFromCurrency}
-                            options={options}
-                            onChange={this.handleSelectChange}
-                        />
-                    </div>
-                    <TextField
-                        label=""
-                        value={fromAmount}
-                        onChange={this.handleChange('fromAmount')}
-                        type="text"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    {currencyJSON[selectedFromCurrency.value].symbol}
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
+                <div>
                     <div>
-                        {selectedFromCurrency.value}
+                        <div className={classes.flagSvgContainer}>
+                            {selectedFromCurrency.flagSrc != null
+                                ? (
+                                    <ReactSVG
+                                        src={selectedFromCurrency.flagSrc}
+                                        className={classes.flagSvg}
+                                    />
+                                )
+                                : null
+                            }
+                        </div>
                     </div>
-                </Card>
+                    <Card className={classes.card}>
+                        <div className={classes.select}>
+                            <Select
+                                className="currency"
+                                classNamePrefix="select"
+                                name="selectedFromCurrency"
+                                defaultValue={selectedFromCurrency}
+                                options={options}
+                                onChange={this.handleSelectChange}
+                            />
+                        </div>
+                        <TextField
+                            label=""
+                            value={fromAmount}
+                            onChange={this.handleChange('fromAmount')}
+                            type="text"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            margin="normal"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        {currencyJSON[selectedFromCurrency.value].symbol}
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <div>
+                            {selectedFromCurrency.value}
+                        </div>
+                    </Card>
+                </div>
 
                 <div className={classes.divider}/>
 
-                <Card className={classes.card}>
-                    <div className={classes.select}>
-                        <Select
-                            className="currency"
-                            classNamePrefix="select"
-                            name="selectedToCurrency"
-                            defaultValue={selectedToCurrency}
-                            options={options}
-                            onChange={this.handleSelectChange}
-                        />
-                    </div>
-                    <TextField
-                        label=""
-                        value={toAmount}
-                        onChange={this.handleChange('toAmount')}
-                        type="text"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    {currencyJSON[selectedToCurrency.value].symbol}
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
+                <div>
                     <div>
-                        {selectedToCurrency.value}
+                        <div className={classes.flagSvgContainer}>
+                            {selectedToCurrency.flagSrc != null
+                                ? (
+                                    <ReactSVG
+                                        src={selectedToCurrency.flagSrc}
+                                        className={classes.flagSvg}
+                                    />
+                                )
+                                : null
+                            }
+                        </div>
                     </div>
-                </Card>
+                    <Card className={classes.card}>
+                        <div className={classes.select}>
+                            <Select
+                                className="currency"
+                                classNamePrefix="select"
+                                name="selectedToCurrency"
+                                defaultValue={selectedToCurrency}
+                                options={options}
+                                onChange={this.handleSelectChange}
+                            />
+                        </div>
+                        <TextField
+                            label=""
+                            value={toAmount}
+                            onChange={this.handleChange('toAmount')}
+                            type="text"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            margin="normal"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        {currencyJSON[selectedToCurrency.value].symbol}
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <div>
+                            {selectedToCurrency.value}
+                        </div>
+                    </Card>
+                </div>
             </div>
         );
     }
@@ -191,7 +224,9 @@ class Root extends React.Component {
 
 
     handleSelectChange = async (value, data) => {
-        await this.setStateAsync({[data.name]: value});
+        // changing the selected currency for the specified select element
+        // also changing the flagSrc attribute
+        await this.setStateAsync({[data.name]: {...value, flagSrc: flagJSON[value.value].src}});
         const {selectedFromCurrency, selectedToCurrency} = this.state;
 
         let api = new ConverterAPI(selectedFromCurrency.value, selectedToCurrency.value);
